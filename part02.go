cbf0b4d2ad29e6f0234bb53db08346e6e0032c54
henry slater <henry.slater@gmail.com>
2020-12-01 22:35:12 +0000

    Day 1

---
+++ part02.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+    "bufio"
+    "log"
+    "os"
+    "strconv"
+    "container/list"
+    "fmt"
+)
+
+func main() {
+    file, err := os.Open("input.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    l := list.New()
+    for scanner.Scan() {
+        value, _ := strconv.Atoi(scanner.Text())
+        l.PushBack(value)
+    }
+
+    
+    for it := l.Front(); it != nil; it = it.Next() {
+        for jt := it.Next(); jt != nil; jt = jt.Next() {
+            for kt := jt.Next(); kt != nil; kt = kt.Next() {
+                if it.Value.(int) + jt.Value.(int) + kt.Value.(int) == 2020 {
+                    fmt.Println(it.Value.(int)*jt.Value.(int)*kt.Value.(int))
+                }
+            }
+        }
+    }
+}
