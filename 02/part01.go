fbb3470f43aaebc31d935692c6d0147c25622706
henry slater <henry.slater@gmail.com>
2020-12-02 21:56:47 +0000

    Day 2

---
+++ 02/part01.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "bufio"
+    "log"
+    "os"
+    "regexp"
+    "fmt"
+    "strconv"
+    "strings"
+)
+
+func main() {
+    file, err := os.Open("input.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    valid_count := 0
+    scanner := bufio.NewScanner(file)
+    re := regexp.MustCompile("(?P<min>[0-9]+)-(?P<max>[0-9]+) (?P<char>[a-z]): (?P<pass>[a-z]+)+")
+    for scanner.Scan() {
+        line := scanner.Text()
+        matches := re.FindStringSubmatch(line)
+        min, _ := strconv.Atoi(matches[re.SubexpIndex("min")])
+        max, _ := strconv.Atoi(matches[re.SubexpIndex("max")])
+        char := matches[re.SubexpIndex("char")]
+        pass := matches[re.SubexpIndex("pass")]
+
+        count := strings.Count(pass,char);
+        if count >= min && count <= max {
+            valid_count++
+        }
+
+    }
+    fmt.Println(valid_count)
+}
---
+++ 02/part02.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+    "bufio"
+    "log"
+    "os"
+    "regexp"
+    "fmt"
+    "strconv"
+)
+
+func main() {
+    file, err := os.Open("input.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    valid_count := 0
+    scanner := bufio.NewScanner(file)
+    re := regexp.MustCompile("(?P<min>[0-9]+)-(?P<max>[0-9]+) (?P<char>[a-z]): (?P<pass>[a-z]+)+")
+    for scanner.Scan() {
+        line := scanner.Text()
+        matches := re.FindStringSubmatch(line)
+        min, _ := strconv.Atoi(matches[re.SubexpIndex("min")])
+        max, _ := strconv.Atoi(matches[re.SubexpIndex("max")])
+        char := matches[re.SubexpIndex("char")]
+        pass := matches[re.SubexpIndex("pass")]+"                                        "
+
+        if (pass[min-1] == char[0]) != (pass[max-1] == char[0]) {
+            valid_count++
+        }
+
+    }
+    fmt.Println(valid_count)
+}
