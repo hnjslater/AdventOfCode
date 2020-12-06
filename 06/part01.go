81bfc72250a304fcec5a12bc0e4789a8a193082d
henry slater <henry.slater@gmail.com>
2020-12-06 11:26:39 +0000

    Day 6

---
+++ 06/part01.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+    "bufio"
+    "log"
+    "os"
+    "fmt"
+)
+
+func main() {
+    file, err := os.Open("input.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    answers := make(map[rune]bool)
+    total := 0
+    for scanner.Scan() {
+        line := scanner.Text()
+        if line != "" {
+            for _,r := range line {
+                answers[r] = true
+            }
+        } else {
+            total += len(answers)
+            answers = make(map[rune]bool)
+        }
+    }
+    total += len(answers)
+    fmt.Println(total)
+    
+}
