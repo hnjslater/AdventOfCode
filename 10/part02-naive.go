7deef903c0e3fa52b5e48f144cab517ddd462501
henry slater <henry.slater@gmail.com>
2020-12-12 02:28:00 +0000

    Day 10

--- 10/part02-naive.go
+++ 10/part02-naive.go
@@ -1,33 +1,45 @@
 package main
 
+// After the fact, I came back and fixed my naive solution.
+
 import (
 	"bufio"
 	"flag"
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
-        "sort"
 )
 
 var input_file = flag.String("input", "input.txt", "Input file")
 
-func CountSubtrees(start int, buffer []int) int {
-    if len(buffer)-start == 1 {
-        return 1
-    }
+func CountSubtrees2(start int, buffer []int, cache map[int]int) int {
+	value, found := cache[start]
+	if found {
+		return value
+	}
+	if len(buffer)-start == 1 {
+		return 1
+	}
 
-    total := 0
-    if buffer[start+1] - buffer[start] <= 3 {
-        total += CountSubtrees(start+1, buffer)
-    }
-    if len(buffer)-start > 2 && buffer[start+2] - buffer[start] <= 3 {
-        total += CountSubtrees(start+2, buffer)
-    }
-    if len(buffer)-start > 3 && buffer[start+3] - buffer[start] <= 3 {
-        total += CountSubtrees(start+3, buffer)
-    }
-    return total
+	total := 0
+	if buffer[start+1]-buffer[start] <= 3 {
+		total += CountSubtrees2(start+1, buffer, cache)
+	}
+	if len(buffer)-start > 2 && buffer[start+2]-buffer[start] <= 3 {
+		total += CountSubtrees2(start+2, buffer, cache)
+	}
+	if len(buffer)-start > 3 && buffer[start+3]-buffer[start] <= 3 {
+		total += CountSubtrees2(start+3, buffer, cache)
+	}
+	cache[start] = total
+	return total
+}
+
+func CountSubtrees(start int, buffer []int) int {
+	cache := make(map[int]int)
+	return CountSubtrees2(start, buffer, cache)
 }
 
 func main() {
@@ -40,19 +52,17 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	var buffer []int
-        buffer = append(buffer, 0)
+	buffer = append(buffer, 0)
 	for scanner.Scan() {
 		line := scanner.Text()
 		value, err := strconv.Atoi(line)
 		if err != nil {
 			log.Fatal("Error parsing input")
 		}
-                buffer = append(buffer, value)
+		buffer = append(buffer, value)
 	}
-        sort.Ints(buffer)
-        buffer = append(buffer, buffer[len(buffer)-1]+3)
+	sort.Ints(buffer)
+	buffer = append(buffer, buffer[len(buffer)-1]+3)
 
-        fmt.Println(CountSubtrees(0,buffer))
-
-
+	fmt.Println(CountSubtrees(0, buffer))
 }
