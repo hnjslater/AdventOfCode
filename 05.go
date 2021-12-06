87db3a016cff3bc33d52e2320182cd107a43fdc1
Henry Nicholas Joseph Slater <henryslater@google.com>
2021-12-06 15:25:25 +0000

    Day 6: Lanternfish

--- 05.go
+++ 05.go
@@ -22,44 +22,30 @@ func main() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-
 	scanner.Scan()
 	line := scanner.Text()
-	var fish []int
-	for _,entry := range strings.Split(line, ",") {
-		x,_ := strconv.Atoi(entry)
-		fish = append(fish, x)
-	}
-	fmt.Println(fish)
-	if *part == 1 {
-		for g := 0; g < 256; g++ {
-			for i := 0; i < len(fish); i++ {
-				fish[i]-= 1
-				if fish[i] == -1 {
-				   fish[i] = 6
-				   fish = append(fish, 9)
-			   }
-			}
-		}
-	} else {
-		var fishies [10]int
-		for _,ent := range fish {
-			fishies[ent]++
-		}
 
-		for g := 0; g < 256; g++ {
-			breeders := fishies[0]
-			for i := 1; i <= 9; i++ {
-				fishies[i-1] = fishies[i]
-			}
-			fishies[8] = breeders
-			fishies[6] += breeders
-		}
-		pop := 0
-		for _,e := range fishies {
-			pop += e
-		}
-		fmt.Println(pop)
+	var fishies [9]int
+	for _, entry := range strings.Split(line, ",") {
+		x, _ := strconv.Atoi(entry)
+		fishies[x]++
+	}
+	generations := 80
+	if *part == 2 {
+		generations = 256
 	}
 
+	for g := 0; g < generations; g++ {
+		breeders := fishies[0]
+		for i := 1; i <= 8; i++ {
+			fishies[i-1] = fishies[i]
+		}
+		fishies[8] = breeders
+		fishies[6] += breeders
+	}
+	pop := 0
+	for _, e := range fishies {
+		pop += e
+	}
+	fmt.Println(pop)
 }
